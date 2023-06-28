fac315576e0e3cae0786f84cc3c965f9170a33fe
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-06-28 19:05:08 -0400

    get apod in main viewmodel

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,29 @@
 package com.udacity.asteroidradar.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.BuildConfig
+import com.udacity.asteroidradar.PictureOfDay
+import com.udacity.asteroidradar.network.Network
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class MainViewModel : ViewModel() {
+
+    private val _picture = MutableLiveData<PictureOfDay>()
+
+    val picture: LiveData<PictureOfDay>
+        get() = _picture
+
+    init {
+        viewModelScope.launch {
+            try {
+                _picture.value = Network.apod.getPictureOfTheDay(BuildConfig.API_KEY)
+            } catch (e: Exception) {
+                e.message?.let { Timber.i(it) }
+            }
+        }
+    }
 }
