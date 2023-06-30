86f78a1372426464c2bd4430ad5d123036aeb0a1
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-06-29 22:36:37 -0400

    fetch asteroids list from api

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -4,25 +4,34 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.BuildConfig
 import com.udacity.asteroidradar.PictureOfDay
-import com.udacity.asteroidradar.network.Network
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import com.udacity.asteroidradar.network.ApodApi
+import com.udacity.asteroidradar.network.AsteroidApi
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class MainViewModel : ViewModel() {
 
+    private val _asteroids = MutableLiveData<List<Asteroid>>()
     private val _picture = MutableLiveData<PictureOfDay>()
 
+    val asteroids: LiveData<List<Asteroid>>
+        get() = _asteroids
     val picture: LiveData<PictureOfDay>
         get() = _picture
 
     init {
         viewModelScope.launch {
             try {
-                _picture.value = Network.apod.getPictureOfTheDay(BuildConfig.API_KEY)
+                _picture.value = ApodApi.apodService.getPictureOfTheDay(BuildConfig.API_KEY)
+                val rawJson = AsteroidApi.asteroidService.getFeed("2023-06-22", "2023-06-29", BuildConfig.API_KEY)
+                _asteroids.value = parseAsteroidsJsonResult(rawJson)
+                Timber.i("first asteroid: ${_asteroids.value?.first()?.codename}")
             } catch (e: Exception) {
-                e.message?.let { Timber.i(it) }
+                e.message?.let { Timber.e(it) }
             }
         }
     }
