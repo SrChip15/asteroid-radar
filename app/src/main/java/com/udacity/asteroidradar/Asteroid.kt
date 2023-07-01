df5bcb9dbc7c4a00c49240d4b8a1dea68eab79bb
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-01 00:57:45 -0400

    update asteroid model

--- app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -8,10 +8,35 @@ import kotlinx.parcelize.Parcelize
 data class Asteroid(
     val id: Long,
     @Json(name = "name") val codename: String,
-    val closeApproachDate: String,
-    val absoluteMagnitude: Double,
-    val estimatedDiameter: Double,
-    val relativeVelocity: Double,
-    val distanceFromEarth: Double,
-    val isPotentiallyHazardous: Boolean,
+    @Json (name = "close_approach_data") val closeApproachData: List<CloseApproachData>,
+    @Json(name = "estimated_diameter") val estimatedDiameterData: EstimatedDiameterData,
+    @Json(name = "absolute_magnitude_h") val absoluteMagnitude: Double,
+    @Json(name = "is_potentially_hazardous_asteroid") val isPotentiallyHazardous: Boolean,
 ) : Parcelable
+
+@Parcelize
+data class EstimatedDiameterData(
+    @Json(name = "kilometers") val estimatedDiameterInKilometers: EstimatedDiameterInKilometers
+): Parcelable
+
+@Parcelize
+data class EstimatedDiameterInKilometers(
+    @Json(name = "estimated_diameter_max") val estimatedDiameter: Double
+): Parcelable
+
+@Parcelize
+data class CloseApproachData(
+    @Json(name = "close_approach_date") val closeApproachDate: String,
+    @Json(name = "relative_velocity") val relativeVelocityData: RelativeVelocityData,
+    @Json(name = "miss_distance") val missDistance: CloseApproachMissDistance,
+): Parcelable
+
+@Parcelize
+data class RelativeVelocityData(
+    @Json(name = "kilometers_per_second") val relativeVelocity: Double
+): Parcelable
+
+@Parcelize
+data class CloseApproachMissDistance(
+    @Json(name = "astronomical") val distanceFromEarth: Double,
+): Parcelable
