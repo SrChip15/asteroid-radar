2716d84224982545c93e60665e7b6ff2b3face4b
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-02 21:32:00 -0400

    document nasa service network module

--- app/src/main/java/com/udacity/asteroidradar/network/NasaService.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/NasaService.kt
@@ -5,13 +5,25 @@ import retrofit2.http.GET
 
 interface NasaService {
 
+    /**
+     * Retrofit service to fetch astronomy picture of the day ('apod')
+     * from the apod service, which is used in the main view UI
+     */
     @GET("planetary/apod")
     suspend fun getPictureOfTheDay(): PictureOfDay
 
+    /**
+     * Retrofit service to fetch a list of asteroids from the NeoWs service
+     * and this is used to display the list of asteroids in the recycler view
+     * in the main view UI
+     */
     @GET("neo/rest/v1/feed")
     suspend fun getFeed(): NeoWsResponse
 }
 
+/**
+ * Provides Singleton object access to all callers requiring network service.
+ */
 object NasaApi {
     val nasaService: NasaService by lazy {
         Client.getInstance().create(NasaService::class.java)
