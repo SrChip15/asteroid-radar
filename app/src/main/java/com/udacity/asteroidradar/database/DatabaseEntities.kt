a4d63f94d0b99d22716248f76ace102e9fc39e84
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-03 23:44:33 -0400

    add domain model converter ext fun for db asteroid

--- app/src/main/java/com/udacity/asteroidradar/database/DatabaseEntities.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/DatabaseEntities.kt
@@ -2,13 +2,15 @@ package com.udacity.asteroidradar.database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.udacity.asteroidradar.domain.Asteroid
+import com.udacity.asteroidradar.util.dateAsDateString
 
 @Entity
 data class DatabaseAsteroid constructor(
     @PrimaryKey
     val id: Long,
     val codename: String,
-    val closeApproachDate: String,
+    val closeApproachDate: Long,
     val absoluteMagnitude: Double,
     val estimatedDiameter: Double,
     val relativeVelocity: Double,
@@ -23,3 +25,18 @@ data class DatabasePictureOfDay constructor(
     val title: String,
     val url: String,
 )
+
+fun List<DatabaseAsteroid>.asDomainModel(): List<Asteroid> {
+    return map {
+        Asteroid(
+            id = it.id,
+            codename = it.codename,
+            closeApproachDate = dateAsDateString(it.closeApproachDate),
+            absoluteMagnitude = it.absoluteMagnitude,
+            estimatedDiameter = it.estimatedDiameter,
+            relativeVelocity = it.relativeVelocity,
+            distanceFromEarth = it.distanceFromEarth,
+            isPotentiallyDangerous = it.isPotentiallyDangerous,
+        )
+    }
+}
