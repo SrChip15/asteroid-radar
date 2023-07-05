7196351e1ad42744c3ba32778b541d85836e3762
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-05 00:13:00 -0400

    setup work manager with repeating request

--- app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -1,11 +1,65 @@
 package com.udacity.asteroidradar
 
 import android.app.Application
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.udacity.asteroidradar.work.RefreshDataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
-class AsteroidRadarApplication: Application() {
+class AsteroidRadarApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    /**
+     * onCreate is called before the first screen is shown to the user.
+     *
+     * Use it to setup any background tasks, running expensive setup operations in a background
+     * thread to avoid delaying app start.
+     */
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresCharging(true)
+            .setRequiresBatteryNotLow(true)
+            .apply {
+                if (VERSION.SDK_INT >= VERSION_CODES.O) {
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(
+            1,
+            TimeUnit.DAYS
+        ).setConstraints(constraints).build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            RefreshDataWork.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+
     }
 }
