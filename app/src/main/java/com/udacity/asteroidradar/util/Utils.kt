c82ab2f12cfe8a57ebc70fdf0da9c4571dab3083
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-05 22:36:49 -0400

    add util functions

--- app/src/main/java/com/udacity/asteroidradar/util/Utils.kt
+++ app/src/main/java/com/udacity/asteroidradar/util/Utils.kt
@@ -1,10 +1,9 @@
 package com.udacity.asteroidradar.util
 
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
-
-
 
 fun dateStringAsDate(dateString: String): Long {
     val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
@@ -14,4 +13,22 @@ fun dateStringAsDate(dateString: String): Long {
 fun dateAsDateString(timeInMillis: Long): String {
     val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
     return dateFormat.format(Date(timeInMillis))
+}
+
+fun getToday(): String {
+    val calendar = Calendar.getInstance()
+    val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+    val currentTime = calendar.time
+
+    return dateFormat.format(currentTime)
+}
+
+fun getTomorrow(): String {
+    val calendar = Calendar.getInstance()
+    val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+
+    calendar.add(Calendar.DAY_OF_YEAR, 1)
+    val tomorrowTime = calendar.time
+
+    return dateFormat.format(tomorrowTime)
 }
